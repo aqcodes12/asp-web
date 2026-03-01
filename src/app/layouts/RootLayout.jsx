d62027ea7fcd72b194c6fdb4b32d2b3134b442e6
Abdul Qayyum <aqcodes12@gmail.com>
2026-03-01 17:38:08 +0530

    aq

--- src/app/layouts/RootLayout.jsx
+++ src/app/layouts/RootLayout.jsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { CartDrawer } from "../components/CartDrawer";
+import { ScrollToTop } from "../components/ScrollToTop";
 import { CartProvider } from "../context/CartContext";
 function RootLayout() {
   return <CartProvider>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">
